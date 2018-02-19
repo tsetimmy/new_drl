eed2270c788cffc5eaf1a25038c4118ab7f07670
tsetimmy <timmy.tse1@gmail.com>
2018-02-18 22:51:57 -0500

    changed sh files little bit

--- prototype8/continuous_action/go_ddpg.sh
+++ prototype8/continuous_action/go_ddpg.sh
@@ -1,13 +1,13 @@
-python ddpg.py --environment=Reacher-v1 --epochs=500 |tee ./experiments_data/Reacher_ddpg_1.txt
-python ddpg.py --environment=Pusher-v0 --epochs=500 |tee ./experiments_data/Pusher_ddpg_1.txt
-python ddpg.py --environment=Thrower-v0 --epochs=500 |tee ./experiments_data/Thrower_ddpg_1.txt
-python ddpg.py --environment=Striker-v0 --epochs=500 |tee ./experiments_data/Striker_ddpg_1.txt
-python ddpg.py --environment=InvertedPendulum-v1 --epochs=500 |tee ./experiments_data/InvertedPendulum_ddpg_1.txt
-python ddpg.py --environment=InvertedDoublePendulum-v1 --epochs=500 |tee ./experiments_data/InvertedDoublePendulum_ddpg_1.txt
-python ddpg.py --environment=HalfCheetah-v1 --epochs=500 |tee ./experiments_data/HalfCheetah_ddpg_1.txt
-python ddpg.py --environment=Hopper-v1 --epochs=500 |tee ./experiments_data/Hopper_ddpg_1.txt
-python ddpg.py --environment=Swimmer-v1 --epochs=500 |tee ./experiments_data/Swimmer_ddpg_1.txt
-python ddpg.py --environment=Walker2d-v1 --epochs=500 |tee ./experiments_data/Walker2d_ddpg_1.txt
-python ddpg.py --environment=Ant-v1 --epochs=500 |tee ./experiments_data/Ant_ddpg_1.txt
-python ddpg.py --environment=Humanoid-v1 --epochs=500 |tee ./experiments_data/Humanoid_ddpg_1.txt
+python ddpg.py --environment=Reacher-v1 --epochs=500 |tee ./experiments_data/Reacher_ddpg_1.txt &&
+python ddpg.py --environment=Pusher-v0 --epochs=500 |tee ./experiments_data/Pusher_ddpg_1.txt &&
+python ddpg.py --environment=Thrower-v0 --epochs=500 |tee ./experiments_data/Thrower_ddpg_1.txt &&
+python ddpg.py --environment=Striker-v0 --epochs=500 |tee ./experiments_data/Striker_ddpg_1.txt &&
+python ddpg.py --environment=InvertedPendulum-v1 --epochs=500 |tee ./experiments_data/InvertedPendulum_ddpg_1.txt &&
+python ddpg.py --environment=InvertedDoublePendulum-v1 --epochs=500 |tee ./experiments_data/InvertedDoublePendulum_ddpg_1.txt &&
+python ddpg.py --environment=HalfCheetah-v1 --epochs=500 |tee ./experiments_data/HalfCheetah_ddpg_1.txt &&
+python ddpg.py --environment=Hopper-v1 --epochs=500 |tee ./experiments_data/Hopper_ddpg_1.txt &&
+python ddpg.py --environment=Swimmer-v1 --epochs=500 |tee ./experiments_data/Swimmer_ddpg_1.txt &&
+python ddpg.py --environment=Walker2d-v1 --epochs=500 |tee ./experiments_data/Walker2d_ddpg_1.txt &&
+python ddpg.py --environment=Ant-v1 --epochs=500 |tee ./experiments_data/Ant_ddpg_1.txt &&
+python ddpg.py --environment=Humanoid-v1 --epochs=500 |tee ./experiments_data/Humanoid_ddpg_1.txt &&
 python ddpg.py --environment=HumanoidStandup-v1 --epochs=500 |tee ./experiments_data/HumanoidStandup_ddpg_1.txt
--- prototype8/continuous_action/go_gan.sh
+++ prototype8/continuous_action/go_gan.sh
@@ -1,13 +1,13 @@
-python joint.py --environment=Reacher-v1 --model=gan --epochs=500 |tee ./experiments_data/Reacher_gan_1.txt
-python joint.py --environment=Pusher-v0 --model=gan --epochs=500 |tee ./experiments_data/Pusher_gan_1.txt
-python joint.py --environment=Thrower-v0 --model=gan --epochs=500 |tee ./experiments_data/Thrower_gan_1.txt
-python joint.py --environment=Striker-v0 --model=gan --epochs=500 |tee ./experiments_data/Striker_gan_1.txt
-python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=500 |tee ./experiments_data/InvertedPendulum_gan_1.txt
-python joint.py --environment=InvertedDoublePendulum-v1 --model=gan --epochs=500 |tee ./experiments_data/InvertedDoublePendulum_gan_1.txt
-python joint.py --environment=HalfCheetah-v1 --model=gan --epochs=500 |tee ./experiments_data/HalfCheetah_gan_1.txt
-python joint.py --environment=Hopper-v1 --model=gan --epochs=500 |tee ./experiments_data/Hopper_gan_1.txt
-python joint.py --environment=Swimmer-v1 --model=gan --epochs=500 |tee ./experiments_data/Swimmer_gan_1.txt
-python joint.py --environment=Walker2d-v1 --model=gan --epochs=500 |tee ./experiments_data/Walker2d_gan_1.txt
-python joint.py --environment=Ant-v1 --model=gan --epochs=500 |tee ./experiments_data/Ant_gan_1.txt
-python joint.py --environment=Humanoid-v1 --model=gan --epochs=500 |tee ./experiments_data/Humanoid_gan_1.txt
+python joint.py --environment=Reacher-v1 --model=gan --epochs=500 |tee ./experiments_data/Reacher_gan_1.txt &&
+python joint.py --environment=Pusher-v0 --model=gan --epochs=500 |tee ./experiments_data/Pusher_gan_1.txt &&
+python joint.py --environment=Thrower-v0 --model=gan --epochs=500 |tee ./experiments_data/Thrower_gan_1.txt &&
+python joint.py --environment=Striker-v0 --model=gan --epochs=500 |tee ./experiments_data/Striker_gan_1.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=500 |tee ./experiments_data/InvertedPendulum_gan_1.txt &&
+python joint.py --environment=InvertedDoublePendulum-v1 --model=gan --epochs=500 |tee ./experiments_data/InvertedDoublePendulum_gan_1.txt &&
+python joint.py --environment=HalfCheetah-v1 --model=gan --epochs=500 |tee ./experiments_data/HalfCheetah_gan_1.txt &&
+python joint.py --environment=Hopper-v1 --model=gan --epochs=500 |tee ./experiments_data/Hopper_gan_1.txt &&
+python joint.py --environment=Swimmer-v1 --model=gan --epochs=500 |tee ./experiments_data/Swimmer_gan_1.txt &&
+python joint.py --environment=Walker2d-v1 --model=gan --epochs=500 |tee ./experiments_data/Walker2d_gan_1.txt &&
+python joint.py --environment=Ant-v1 --model=gan --epochs=500 |tee ./experiments_data/Ant_gan_1.txt &&
+python joint.py --environment=Humanoid-v1 --model=gan --epochs=500 |tee ./experiments_data/Humanoid_gan_1.txt &&
 python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=500 |tee ./experiments_data/HumanoidStandup_gan_1.txt
