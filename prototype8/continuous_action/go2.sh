3209ba98568c4cb297ca5e5c06bde3e8c22604a3
tsetimmy <timmy.tse1@gmail.com>
2018-02-25 17:53:42 -0500

    changed script

--- prototype8/continuous_action/go2.sh
+++ prototype8/continuous_action/go2.sh
@@ -1,39 +1,39 @@
-python ddpg.py --environment=Pusher-v0 --epochs=1000 |tee ./experiments_data/Pusher_ddpg_2.txt &&
-python joint.py --environment=Pusher-v0 --model=gan --epochs=1000 |tee ./experiments_data/Pusher_gan_2.txt &&
-python joint.py --environment=Pusher-v0 --model=gated --epochs=1000 |tee ./experiments_data/Pusher_gated_2.txt &&
-python ddpg.py --environment=Thrower-v0 --epochs=1000 |tee ./experiments_data/Thrower_ddpg_2.txt &&
-python joint.py --environment=Thrower-v0 --model=gan --epochs=1000 |tee ./experiments_data/Thrower_gan_2.txt &&
-python joint.py --environment=Thrower-v0 --model=gated --epochs=1000 |tee ./experiments_data/Thrower_gated_2.txt &&
-python ddpg.py --environment=Striker-v0 --epochs=1000 |tee ./experiments_data/Striker_ddpg_2.txt &&
-python joint.py --environment=Striker-v0 --model=gan --epochs=1000 |tee ./experiments_data/Striker_gan_2.txt &&
-python joint.py --environment=Striker-v0 --model=gated --epochs=1000 |tee ./experiments_data/Striker_gated_2.txt &&
-python ddpg.py --environment=InvertedPendulum-v1 --epochs=1000 |tee ./experiments_data/InvertedPendulum_ddpg_2.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=1000 |tee ./experiments_data/InvertedPendulum_gan_2.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=1000 |tee ./experiments_data/InvertedPendulum_gated_2.txt &&
-python ddpg.py --environment=Humanoid-v1 --epochs=1000 |tee ./experiments_data/Humanoid_ddpg_2.txt &&
-python joint.py --environment=Humanoid-v1 --model=gan --epochs=1000 |tee ./experiments_data/Humanoid_gan_2.txt &&
-python joint.py --environment=Humanoid-v1 --model=gated --epochs=1000 |tee ./experiments_data/Humanoid_gated_2.txt &&
-python ddpg.py --environment=HumanoidStandup-v1 --epochs=1000 |tee ./experiments_data/HumanoidStandup_ddpg_2.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=1000 |tee ./experiments_data/HumanoidStandup_gan_2.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=1000 |tee ./experiments_data/HumanoidStandup_gated_2.txt
+python ddpg.py --environment=Pusher-v0 --epochs=3000 |tee ./experiments_data/Pusher_ddpg_2.txt &&
+python joint.py --environment=Pusher-v0 --model=gan --epochs=3000 |tee ./experiments_data/Pusher_gan_2.txt &&
+python joint.py --environment=Pusher-v0 --model=gated --epochs=3000 |tee ./experiments_data/Pusher_gated_2.txt &&
+python ddpg.py --environment=Thrower-v0 --epochs=3000 |tee ./experiments_data/Thrower_ddpg_2.txt &&
+python joint.py --environment=Thrower-v0 --model=gan --epochs=3000 |tee ./experiments_data/Thrower_gan_2.txt &&
+python joint.py --environment=Thrower-v0 --model=gated --epochs=3000 |tee ./experiments_data/Thrower_gated_2.txt &&
+python ddpg.py --environment=Striker-v0 --epochs=3000 |tee ./experiments_data/Striker_ddpg_2.txt &&
+python joint.py --environment=Striker-v0 --model=gan --epochs=3000 |tee ./experiments_data/Striker_gan_2.txt &&
+python joint.py --environment=Striker-v0 --model=gated --epochs=3000 |tee ./experiments_data/Striker_gated_2.txt &&
+python ddpg.py --environment=InvertedPendulum-v1 --epochs=3000 |tee ./experiments_data/InvertedPendulum_ddpg_2.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=3000 |tee ./experiments_data/InvertedPendulum_gan_2.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=3000 |tee ./experiments_data/InvertedPendulum_gated_2.txt &&
+python ddpg.py --environment=Humanoid-v1 --epochs=3000 |tee ./experiments_data/Humanoid_ddpg_2.txt &&
+python joint.py --environment=Humanoid-v1 --model=gan --epochs=3000 |tee ./experiments_data/Humanoid_gan_2.txt &&
+python joint.py --environment=Humanoid-v1 --model=gated --epochs=3000 |tee ./experiments_data/Humanoid_gated_2.txt &&
+python ddpg.py --environment=HumanoidStandup-v1 --epochs=3000 |tee ./experiments_data/HumanoidStandup_ddpg_2.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=3000 |tee ./experiments_data/HumanoidStandup_gan_2.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=3000 |tee ./experiments_data/HumanoidStandup_gated_2.txt
 
-python ddpg.py --environment=Pusher-v0 --epochs=1000 |tee ./experiments_data/Pusher_ddpg_3.txt &&
-python joint.py --environment=Pusher-v0 --model=gan --epochs=1000 |tee ./experiments_data/Pusher_gan_3.txt &&
-python joint.py --environment=Pusher-v0 --model=gated --epochs=1000 |tee ./experiments_data/Pusher_gated_3.txt &&
-python ddpg.py --environment=Thrower-v0 --epochs=1000 |tee ./experiments_data/Thrower_ddpg_3.txt &&
-python joint.py --environment=Thrower-v0 --model=gan --epochs=1000 |tee ./experiments_data/Thrower_gan_3.txt &&
-python joint.py --environment=Thrower-v0 --model=gated --epochs=1000 |tee ./experiments_data/Thrower_gated_3.txt &&
-python ddpg.py --environment=Striker-v0 --epochs=1000 |tee ./experiments_data/Striker_ddpg_3.txt &&
-python joint.py --environment=Striker-v0 --model=gan --epochs=1000 |tee ./experiments_data/Striker_gan_3.txt &&
-python joint.py --environment=Striker-v0 --model=gated --epochs=1000 |tee ./experiments_data/Striker_gated_3.txt &&
-python ddpg.py --environment=InvertedPendulum-v1 --epochs=1000 |tee ./experiments_data/InvertedPendulum_ddpg_3.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=1000 |tee ./experiments_data/InvertedPendulum_gan_3.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=1000 |tee ./experiments_data/InvertedPendulum_gated_3.txt &&
-python ddpg.py --environment=Humanoid-v1 --epochs=1000 |tee ./experiments_data/Humanoid_ddpg_3.txt &&
-python joint.py --environment=Humanoid-v1 --model=gan --epochs=1000 |tee ./experiments_data/Humanoid_gan_3.txt &&
-python joint.py --environment=Humanoid-v1 --model=gated --epochs=1000 |tee ./experiments_data/Humanoid_gated_3.txt &&
-python ddpg.py --environment=HumanoidStandup-v1 --epochs=1000 |tee ./experiments_data/HumanoidStandup_ddpg_3.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=1000 |tee ./experiments_data/HumanoidStandup_gan_3.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=1000 |tee ./experiments_data/HumanoidStandup_gated_3.txt
+python ddpg.py --environment=Pusher-v0 --epochs=3000 |tee ./experiments_data/Pusher_ddpg_3.txt &&
+python joint.py --environment=Pusher-v0 --model=gan --epochs=3000 |tee ./experiments_data/Pusher_gan_3.txt &&
+python joint.py --environment=Pusher-v0 --model=gated --epochs=3000 |tee ./experiments_data/Pusher_gated_3.txt &&
+python ddpg.py --environment=Thrower-v0 --epochs=3000 |tee ./experiments_data/Thrower_ddpg_3.txt &&
+python joint.py --environment=Thrower-v0 --model=gan --epochs=3000 |tee ./experiments_data/Thrower_gan_3.txt &&
+python joint.py --environment=Thrower-v0 --model=gated --epochs=3000 |tee ./experiments_data/Thrower_gated_3.txt &&
+python ddpg.py --environment=Striker-v0 --epochs=3000 |tee ./experiments_data/Striker_ddpg_3.txt &&
+python joint.py --environment=Striker-v0 --model=gan --epochs=3000 |tee ./experiments_data/Striker_gan_3.txt &&
+python joint.py --environment=Striker-v0 --model=gated --epochs=3000 |tee ./experiments_data/Striker_gated_3.txt &&
+python ddpg.py --environment=InvertedPendulum-v1 --epochs=3000 |tee ./experiments_data/InvertedPendulum_ddpg_3.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=3000 |tee ./experiments_data/InvertedPendulum_gan_3.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=3000 |tee ./experiments_data/InvertedPendulum_gated_3.txt &&
+python ddpg.py --environment=Humanoid-v1 --epochs=3000 |tee ./experiments_data/Humanoid_ddpg_3.txt &&
+python joint.py --environment=Humanoid-v1 --model=gan --epochs=3000 |tee ./experiments_data/Humanoid_gan_3.txt &&
+python joint.py --environment=Humanoid-v1 --model=gated --epochs=3000 |tee ./experiments_data/Humanoid_gated_3.txt &&
+python ddpg.py --environment=HumanoidStandup-v1 --epochs=3000 |tee ./experiments_data/HumanoidStandup_ddpg_3.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=3000 |tee ./experiments_data/HumanoidStandup_gan_3.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=3000 |tee ./experiments_data/HumanoidStandup_gated_3.txt
 
 
--- prototype8/continuous_action/go4.sh
+++ prototype8/continuous_action/go4.sh
@@ -1,18 +1,18 @@
-python ddpg.py --environment=Pusher-v0 --epochs=1000 |tee ./experiments_data/Pusher_ddpg_6.txt &&
-python joint.py --environment=Pusher-v0 --model=gan --epochs=1000 |tee ./experiments_data/Pusher_gan_6.txt &&
-python joint.py --environment=Pusher-v0 --model=gated --epochs=1000 |tee ./experiments_data/Pusher_gated_6.txt &&
-python ddpg.py --environment=Thrower-v0 --epochs=1000 |tee ./experiments_data/Thrower_ddpg_6.txt &&
-python joint.py --environment=Thrower-v0 --model=gan --epochs=1000 |tee ./experiments_data/Thrower_gan_6.txt &&
-python joint.py --environment=Thrower-v0 --model=gated --epochs=1000 |tee ./experiments_data/Thrower_gated_6.txt &&
-python ddpg.py --environment=Striker-v0 --epochs=1000 |tee ./experiments_data/Striker_ddpg_6.txt &&
-python joint.py --environment=Striker-v0 --model=gan --epochs=1000 |tee ./experiments_data/Striker_gan_6.txt &&
-python joint.py --environment=Striker-v0 --model=gated --epochs=1000 |tee ./experiments_data/Striker_gated_6.txt &&
-python ddpg.py --environment=InvertedPendulum-v1 --epochs=1000 |tee ./experiments_data/InvertedPendulum_ddpg_6.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=1000 |tee ./experiments_data/InvertedPendulum_gan_6.txt &&
-python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=1000 |tee ./experiments_data/InvertedPendulum_gated_6.txt &&
-python ddpg.py --environment=Humanoid-v1 --epochs=1000 |tee ./experiments_data/Humanoid_ddpg_6.txt &&
-python joint.py --environment=Humanoid-v1 --model=gan --epochs=1000 |tee ./experiments_data/Humanoid_gan_6.txt &&
-python joint.py --environment=Humanoid-v1 --model=gated --epochs=1000 |tee ./experiments_data/Humanoid_gated_6.txt &&
-python ddpg.py --environment=HumanoidStandup-v1 --epochs=1000 |tee ./experiments_data/HumanoidStandup_ddpg_6.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=1000 |tee ./experiments_data/HumanoidStandup_gan_6.txt &&
-python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=1000 |tee ./experiments_data/HumanoidStandup_gated_6.txt
+python ddpg.py --environment=Pusher-v0 --epochs=3000 |tee ./experiments_data/Pusher_ddpg_6.txt &&
+python joint.py --environment=Pusher-v0 --model=gan --epochs=3000 |tee ./experiments_data/Pusher_gan_6.txt &&
+python joint.py --environment=Pusher-v0 --model=gated --epochs=3000 |tee ./experiments_data/Pusher_gated_6.txt &&
+python ddpg.py --environment=Thrower-v0 --epochs=3000 |tee ./experiments_data/Thrower_ddpg_6.txt &&
+python joint.py --environment=Thrower-v0 --model=gan --epochs=3000 |tee ./experiments_data/Thrower_gan_6.txt &&
+python joint.py --environment=Thrower-v0 --model=gated --epochs=3000 |tee ./experiments_data/Thrower_gated_6.txt &&
+python ddpg.py --environment=Striker-v0 --epochs=3000 |tee ./experiments_data/Striker_ddpg_6.txt &&
+python joint.py --environment=Striker-v0 --model=gan --epochs=3000 |tee ./experiments_data/Striker_gan_6.txt &&
+python joint.py --environment=Striker-v0 --model=gated --epochs=3000 |tee ./experiments_data/Striker_gated_6.txt &&
+python ddpg.py --environment=InvertedPendulum-v1 --epochs=3000 |tee ./experiments_data/InvertedPendulum_ddpg_6.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gan --epochs=3000 |tee ./experiments_data/InvertedPendulum_gan_6.txt &&
+python joint.py --environment=InvertedPendulum-v1 --model=gated --epochs=3000 |tee ./experiments_data/InvertedPendulum_gated_6.txt &&
+python ddpg.py --environment=Humanoid-v1 --epochs=3000 |tee ./experiments_data/Humanoid_ddpg_6.txt &&
+python joint.py --environment=Humanoid-v1 --model=gan --epochs=3000 |tee ./experiments_data/Humanoid_gan_6.txt &&
+python joint.py --environment=Humanoid-v1 --model=gated --epochs=3000 |tee ./experiments_data/Humanoid_gated_6.txt &&
+python ddpg.py --environment=HumanoidStandup-v1 --epochs=3000 |tee ./experiments_data/HumanoidStandup_ddpg_6.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gan --epochs=3000 |tee ./experiments_data/HumanoidStandup_gan_6.txt &&
+python joint.py --environment=HumanoidStandup-v1 --model=gated --epochs=3000 |tee ./experiments_data/HumanoidStandup_gated_6.txt
