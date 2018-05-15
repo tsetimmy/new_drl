3c137ca11cda6cdd6f2447c54882bb5df03b61c5
tsetimmy <timmy.tse1@gmail.com>
2018-05-15 15:18:22 -0400

    Simple bayesian neural net working

--- bayesian_rl_with_edward/bayesian_dynamics_model.py
+++ bayesian_rl_with_edward/bayesian_dynamics_model.py
@@ -5,46 +5,55 @@ import edward as ed
 
 from edward.models import Normal
 
+def rbf(x):
+    return tf.exp(-tf.square(x))
+
 class bayesian_dynamics_model:
     def __init__(self, input_size, output_size):
         self.input_size = input_size
         self.output_size = output_size
-        self.hidden_size = 200
+        self.hidden_size = 20
 
         # Declare placholder.
         self.x = tf.placeholder(shape=[None, self.input_size], dtype=tf.float32)
 
         # Declare weights.
         self.W_0 = Normal(loc=tf.zeros([self.input_size, self.hidden_size]), scale=tf.ones([self.input_size, self.hidden_size]))
-        self.W_1 = Normal(loc=tf.zeros([self.hidden_size, self.output_size]), scale=tf.ones([self.hidden_size, self.output_size]))
+        self.W_1 = Normal(loc=tf.zeros([self.hidden_size, self.hidden_size]), scale=tf.ones([self.hidden_size, self.hidden_size]))
+        self.W_2 = Normal(loc=tf.zeros([self.hidden_size, self.output_size]), scale=tf.ones([self.hidden_size, self.output_size]))
 
         self.b_0 = Normal(loc=tf.zeros(self.hidden_size), scale=tf.ones(self.hidden_size))
-        self.b_1 = Normal(loc=tf.zeros(self.output_size), scale=tf.ones(self.output_size))
+        self.b_1 = Normal(loc=tf.zeros(self.hidden_size), scale=tf.ones(self.hidden_size))
+        self.b_2 = Normal(loc=tf.zeros(self.output_size), scale=tf.ones(self.output_size))
 
         # Output of computational graph.
-        self.y = Normal(loc=self.build(self.x, self.W_0, self.W_1, self.b_0, self.b_1), scale=tf.ones_like(self.x) * .1)
+        nn_out = self.build(self.x, self.W_0, self.W_1, self.W_2, self.b_0, self.b_1, self.b_2)
+        self.y = Normal(loc=nn_out, scale=tf.ones_like(nn_out) * .1)
 
         # Variables.
         self.qW_0 = Normal(loc=tf.get_variable('qW_0/loc', [self.input_size, self.hidden_size]),
                            scale=tf.nn.softplus(tf.get_variable('qW_0/scale', [self.input_size, self.hidden_size])))
-        self.qW_1 = Normal(loc=tf.get_variable('qW_1/loc', [self.hidden_size, self.output_size]),
-                           scale=tf.nn.softplus(tf.get_variable('qW_1/scale', [self.hidden_size, self.output_size])))
+        self.qW_1 = Normal(loc=tf.get_variable('qW_1/loc', [self.hidden_size, self.hidden_size]),
+                           scale=tf.nn.softplus(tf.get_variable('qW_1/scale', [self.hidden_size, self.hidden_size])))
+        self.qW_2 = Normal(loc=tf.get_variable('qW_2/loc', [self.hidden_size, self.output_size]),
+                           scale=tf.nn.softplus(tf.get_variable('qW_2/scale', [self.hidden_size, self.output_size])))
 
         self.qb_0 = Normal(loc=tf.get_variable('qb_0/loc', [self.hidden_size]),
                            scale=tf.nn.softplus(tf.get_variable('qb_0/scale', [self.hidden_size])))
-        self.qb_1 = Normal(loc=tf.get_variable('qb_1/loc', [self.output_size]),
-                           scale=tf.nn.softplus(tf.get_variable('qb_1/scale', [self.output_size])))
+        self.qb_1 = Normal(loc=tf.get_variable('qb_1/loc', [self.hidden_size]),
+                           scale=tf.nn.softplus(tf.get_variable('qb_1/scale', [self.hidden_size])))
+        self.qb_2 = Normal(loc=tf.get_variable('qb_2/loc', [self.output_size]),
+                           scale=tf.nn.softplus(tf.get_variable('qb_2/scale', [self.output_size])))
 
         # Sample functions from variational model to visualize fits.
-        self.mus = self.build(self.x, self.qW_0.sample(), self.qW_1.sample(), self.qb_0.sample(), self.qb_1.sample())
+        self.mus = self.build(self.x, self.qW_0.sample(), self.qW_1.sample(), self.qW_2.sample(), self.qb_0.sample(), self.qb_1.sample(), self.qb_2.sample())
 
-
-
-
-    def build(self, x, W_0, W_1, b_0, b_1):
+    def build(self, x, W_0, W_1, W_2, b_0, b_1, b_2):
         '''Builds the computational graph.'''
-        h_0 = tf.nn.tanh(tf.matmul(x, W_0) + b_0)
-        out = tf.matmul(h_0, W_1) + b_1
+
+        h_0 = rbf(tf.matmul(x, W_0) + b_0)
+        h_1 = rbf(tf.matmul(h_0, W_1) + b_1)
+        out = tf.matmul(h_1, W_2) + b_2
         return out
 
     def generate_toy_data(self, noise_sd=.1, size=50):
@@ -85,7 +94,8 @@ def main():
 
         # Train the model
         inference = ed.KLqp({model.W_0: model.qW_0, model.b_0: model.qb_0,
-                             model.W_1: model.qW_1, model.b_1: model.qb_1}, data={model.x:x, model.y: y})
+                             model.W_1: model.qW_1, model.b_1: model.qb_1,
+                             model.W_2: model.qW_2, model.b_2: model.qb_2}, data={model.x:x, model.y: y})
         inference.run(n_iter=1000, n_samples=5)
 
         # Plot the posterior
