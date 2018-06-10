248769291b14b84fff90ac18bbec905c020bb72d
tsetimmy <timmy.tse1@gmail.com>
2018-06-10 16:11:38 -0400

    Some code cleanup.

--- gaussian_processes/gp_regression.py
+++ gaussian_processes/gp_regression.py
@@ -13,21 +13,30 @@ import sys
 sys.path.append('..')
 from prototype8.dmlac.real_env_pendulum import real_env_pendulum_reward
 
-
 class gp_model:
-    def __init__(self, x_dim, y_dim, no_samples, unroll_steps, x_train_data=None, y_train_data=None):
-        self.state_dim = 3
-        self.action_dim = 1
-        self.discount_factor = .9
-        self.action_bound_low = np.array([-2.])
-        self.action_bound_high = np.array([2.])
+    def __init__(self, x_dim, y_dim, state_dim, action_dim, observation_space_low,
+                 observation_space_high, action_bound_low, action_bound_high, unroll_steps,
+                no_samples, discount_factor, train_policy_batch_size, train_policy_iterations,
+                x_train_data=None, y_train_data=None):
+
+        assert x_dim == state_dim + action_dim
         self.x_dim = x_dim
         self.y_dim = y_dim
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.observation_space_low = observation_space_low
+        self.observation_space_high = observation_space_high
+        self.action_bound_low = action_bound_low
+        self.action_bound_high = action_bound_high
         self.x_train_data = x_train_data
         self.y_train_data = y_train_data
 
-        self.no_samples = no_samples
         self.unroll_steps = unroll_steps
+        self.no_samples = no_samples
+        self.discount_factor = discount_factor
+
+        self.train_policy_batch_size = train_policy_batch_size
+        self.train_policy_iterations = train_policy_iterations
 
         self.policy_scope = 'policy_scope'
         self.policy_reuse_vars = None
@@ -42,20 +51,21 @@ class gp_model:
         self.unroll(self.states)
 
     def train(self, sess, memory):
-
         feed_dict = {}
         for model in self.models:
             feed_dict[model.x_train] = model.x_train_data
             feed_dict[model.y_train] = model.y_train_data
 
-        for it in range(30):
-
-            batch = memory.sample(32)
+        for it in range(self.train_policy_iterations):
+            batch = memory.sample(self.train_policy_batch_size)
             states = np.concatenate([b[0] for b in batch], axis=0)
             feed_dict[self.states] = states
 
-            loss, _ = sess.run([self.loss, self.opt], feed_dict=feed_dict)
-            print 'iteration:', it, 'loss:', loss
+            try:
+                loss, _ = sess.run([self.loss, self.opt], feed_dict=feed_dict)
+                print 'iteration:', it, 'loss:', loss
+            except:
+                print 'Cholesky decomposition failed.'
 
     def act(self, sess, states):
         states = np.atleast_2d(states)
@@ -157,8 +167,20 @@ class gp_model:
                 print 'i:', i, 'iterations:', it, 'loss:', -loss
         '''
 
+    def set_and_train_hyperparameters(self, sess, memory, batch_size, iterations, verbose=True):
+        batch = memory.sample(batch_size)
+
+        states = np.concatenate([b[0] for b in batch], axis=0)
+        actions = np.concatenate([b[1] for b in batch], axis=0)
+        next_states = np.concatenate([b[3] for b in batch], axis=0)
+
+        states_actions = np.concatenate([states, actions], axis=-1)
+
+        self.set_training_data(states_actions, next_states)
+        self.train_hyperparameters(sess, iterations=iterations, verbose=verbose)
+    
     def build_policy(self, states):
-        assert states.shape.as_list() == [None, 3]
+        assert states.shape.as_list() == [None, self.state_dim]
 
         #Fully connected layer 1
         fc1 = slim.fully_connected(states, 256, activation_fn=tf.nn.relu, scope=self.policy_scope+'/fc1', reuse=self.policy_reuse_vars)
@@ -258,10 +280,13 @@ def policy_gradient_experiment():
     import argparse
     from utils import Memory
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-samples", type=int, default=20)
     parser.add_argument("--unroll-steps", type=int, default=25)
+    parser.add_argument("--no-samples", type=int, default=20)
+    parser.add_argument("--discount-factor", type=float, default=.9)
     parser.add_argument("--replay-mem-size", type=int, default=1000000)
-    parser.add_argument("--batch-size", type=int, default=64)
+    parser.add_argument("--train-policy-batch-size", type=int, default=32)
+    parser.add_argument("--train-policy-iterations", type=int, default=30)
+    parser.add_argument("--replay-start-size-epochs", type=int, default=2)
     args = parser.parse_args()
     
     print args
@@ -269,11 +294,22 @@ def policy_gradient_experiment():
     env = gym.make('Pendulum-v0')
 
     # Initialize the agent
-    gpm = gp_model(x_dim=4, y_dim=3, no_samples=args.no_samples, unroll_steps=args.unroll_steps)
+    gpm = gp_model(x_dim=env.observation_space.shape[0]+env.action_space.shape[0],
+                   y_dim=env.observation_space.shape[0],
+                   state_dim=env.observation_space.shape[0],
+                   action_dim=env.action_space.shape[0],
+                   observation_space_low=env.observation_space.low,
+                   observation_space_high=env.observation_space.high,
+                   action_bound_low=env.action_space.low,
+                   action_bound_high=env.action_space.high,
+                   unroll_steps=args.unroll_steps,
+                   no_samples=args.no_samples,
+                   discount_factor=args.discount_factor,
+                   train_policy_batch_size=args.train_policy_batch_size,
+                   train_policy_iterations=args.train_policy_iterations)
 
     # Initialize the memory
     memory = Memory(args.replay_mem_size)
-
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -281,8 +317,8 @@ def policy_gradient_experiment():
         total_rewards = 0.0
         epoch = 1
         for time_steps in range(30000):
-            if epoch <= 2:
-                action = np.random.uniform(-2., 2., 1)
+            if epoch <= args.replay_start_size_epochs:
+                action = np.random.uniform(env.action_space.low, env.action_space.high, 1)
             else:
                 action = gpm.act(sess, state)
             next_state, reward, done, _ = env.step(action)
@@ -297,39 +333,22 @@ def policy_gradient_experiment():
             if done == True:
                 print 'time steps', time_steps, 'epoch', epoch, 'total rewards', total_rewards
 
-                if epoch == 2:
-                    batch = memory.sample(memory.max_size)
+                if epoch == args.replay_start_size_epochs:
+                    gpm.set_and_train_hyperparameters(sess, memory, memory.max_size, 50000, True)
 
-                    states = np.concatenate([b[0] for b in batch], axis=0)
-                    actions = np.concatenate([b[1] for b in batch], axis=0)
-                    next_states = np.concatenate([b[3] for b in batch], axis=0)
-
-                    states_actions = np.concatenate([states, actions], axis=-1)
-                    gpm.set_training_data(states_actions, next_states)
-                    gpm.train_hyperparameters(sess)
-
-                if epoch >= 2:
+                if epoch >= args.replay_start_size_epochs:
                     gpm.train(sess, memory)
 
-                if epoch >= 3:
-                    batch = memory.sample(500)
-
-                    states = np.concatenate([b[0] for b in batch], axis=0)
-                    actions = np.concatenate([b[1] for b in batch], axis=0)
-                    next_states = np.concatenate([b[3] for b in batch], axis=0)
-
-                    states_actions = np.concatenate([states, actions], axis=-1)
-                    gpm.set_training_data(states_actions, next_states)
-                    gpm.train_hyperparameters(sess, 10000, verbose=False)
-
+                if epoch >= args.replay_start_size_epochs + 1:
+                    gpm.set_and_train_hyperparameters(sess, memory, 500, 10000, False)
 
                 epoch += 1
                 total_rewards = 0.
                 state = env.reset()
+
 def main():
     #plotting_experiment()
     policy_gradient_experiment()
-
 
 if __name__ == '__main__':
     main()
